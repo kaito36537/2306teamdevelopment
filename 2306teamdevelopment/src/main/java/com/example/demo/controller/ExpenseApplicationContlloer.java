6375801b8304dfc5a77e814bb79e214aa8b1aa4b
takeru1225 <126079557+takeru1225@users.noreply.github.com>
2023-06-19 23:40:36 +0900

    0619
    0619

--- 2306teamdevelopment/src/main/java/com/example/demo/controller/ExpenseApplicationContlloer.java
+++ 2306teamdevelopment/src/main/java/com/example/demo/controller/ExpenseApplicationContlloer.java
@@ -1,35 +1,23 @@
-[com.example.demo.ExpenseApplicationContlloer]
-
 package com.example.demo.controller;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import com.example.demo.entity.User;
-import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 /**
-* ユーザー情報 Controller
-*/
+ * ユーザー情報 Controller
+ */
 @Controller
-public class UserController {
-/**
-* ユーザー情報 Service
-*/
-//2行追加
-
-/**
-* ユーザー情報一覧画面を表示
-* @param model Model
-* @return ユーザー情報一覧画面のHTML
-*/
-@RequestMapping("/user/list")
-public String displayList(Model model) {
-
-//3行追加
-
-
-
-}
+public class ExpenseController {
+	 
+	 @Autowired
+	  private ExpenseService expenseService;
+	 
+	  @GetMapping(value = "/user/list")
+	  public String displayList(Model model) {
+	    List<User> userlist = userService.searchAll();
+	    model.addAttribute("userlist", userlist);
+	    return "user/list";
+	  }
 }
