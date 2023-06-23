58407af368a53e78abd6084119e22ddde19bc5f3
yutawatanabe0712 <127653923+yutawatanabe0712@users.noreply.github.com>
2023-06-23 23:31:29 +0900

    エラー解決できていない　橋本さんのフロント取り入れ済み

--- 2306teamdevelopment/src/main/java/com/example/demo/mybatis/EmployeeMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.mybatis;
-
-public class EmployeeMapper {
-	@Select({
-	    "select * from employee where name = #{name} limit 1"
-	})
-	Employee selectByName(String name);
-}
